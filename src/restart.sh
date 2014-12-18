86e5161da76312339d1911b8446afc6410117b85
apoliakov <apoliakov@paradigm4.com>
2014-12-18 11:24:45 -0500

    Update restart.sh

--- src/restart.sh
+++ src/restart.sh
@@ -8,6 +8,8 @@ mydir=`dirname $0`
 pushd $mydir
 #Replace this with your 3RDParty directory!
 make SCIDB_3RDPARTY="/opt/scidb/14.12"
+#Replace this with your install directory!
+SCIDB_INSTALL="/opt/scidb/14.12"
 scidb.py stopall $DBNAME 
 cp libload_tools.so $SCIDB_INSTALL/lib/scidb/plugins/
 #for multinode setups, dont forget to copy to every instance
