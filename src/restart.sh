c1ff7567bbcfd8718b272baa92adcec74e1a2bb3
Jonathan Rivers <jrivers@paradigm4.com>
2015-08-18 13:22:25 -0400

    changed name to multisplit

--- src/restart.sh
+++ src/restart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Quick restart script for dev use
-iquery -aq "unload_library('genx')" > /dev/null 2>&1
+iquery -aq "unload_library('multisplit')" > /dev/null 2>&1
 set -e
 
 DBNAME="mydb"
@@ -11,6 +11,8 @@ SCIDB_INSTALL="/development/scidbtrunk/stage/install/"
 make SCIDB_3RDPARTY="/opt/scidb/14.12"
 
 scidb.py stopall $DBNAME 
-cp libgenx.so ${SCIDB_INSTALL}/lib/scidb/plugins/
+cp libmultisplit.so ${SCIDB_INSTALL}/lib/scidb/plugins/
 scidb.py startall $DBNAME 
-iquery -aq "load_library('genx')"
+
+
+iquery -aq "load_library('multisplit')"
