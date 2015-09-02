cd80d5797062f64836e67213c003d98cad91337a
apoliakov <apoliakov@paradigm4.com>
2015-09-02 16:17:38 -0700

    Remove licensing and in the process of rewriting some tests

--- src/ParseSettings.h
+++ src/ParseSettings.h
@@ -1,23 +1,17 @@
 /*
-**
-* BEGIN_COPYRIGHT
-*
-* load_tools is a plugin for SciDB.  Copyright (C) 2008-2014 SciDB, Inc.
-*
-* load_tools is free software: you can redistribute it and/or modify
-* it under the terms of the AFFERO GNU General Public License as published by
-* the Free Software Foundation.
-*
-* load_tools is distributed "AS-IS" AND WITHOUT ANY WARRANTY OF ANY KIND,
-* INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY,
-* NON-INFRINGEMENT, OR FITNESS FOR A PARTICULAR PURPOSE. See
-* the AFFERO GNU General Public License for the complete license terms.
-*
-* You should have received a copy of the AFFERO GNU General Public License
-* along with load_tools.  If not, see <http://www.gnu.org/licenses/agpl-3.0.html>
-*
-* END_COPYRIGHT
-*/
+ **
+ * BEGIN_COPYRIGHT
+ *
+ * PARADIGM4 INC.
+ * This file is part of the Paradigm4 Enterprise SciDB distribution kit
+ * and may only be used with a valid Paradigm4 contract and in accord
+ * with the terms and conditions specified by that contract.
+ *
+ * Copyright (C) 2010 - 2015 Paradigm4 Inc.
+ * All Rights Reserved.
+ *
+ * END_COPYRIGHT
+ */
 
 #include <boost/algorithm/string.hpp>
 #include <boost/lexical_cast.hpp>
