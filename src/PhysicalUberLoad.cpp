e90028533a0d03d41ac6107926e370be2f88f0ff
bwlewis <blewis@illposed.net>
2015-09-22 22:11:18 -0400

    copyright

--- src/PhysicalUberLoad.cpp
+++ src/PhysicalUberLoad.cpp
@@ -2,19 +2,8 @@
 **
 * BEGIN_COPYRIGHT
 *
-* load_tools is a plugin for SciDB.  Copyright (C) 2008-2014 SciDB, Inc.
-*
-* load_tools is free software: you can redistribute it and/or modify
-* it under the terms of the AFFERO GNU General Public License as published by
-* the Free Software Foundation.
-*
-* load_tools is distributed "AS-IS" AND WITHOUT ANY WARRANTY OF ANY KIND,
-* INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY,
-* NON-INFRINGEMENT, OR FITNESS FOR A PARTICULAR PURPOSE. See
-* the AFFERO GNU General Public License for the complete license terms.
-*
-* You should have received a copy of the AFFERO GNU General Public License
-* along with load_tools.  If not, see <http://www.gnu.org/licenses/agpl-3.0.html>
+* load_tools is a plugin for SciDB.  Copyright (C) 2015 SciDB, Inc., all rights
+* reserved
 *
 * END_COPYRIGHT
 */
