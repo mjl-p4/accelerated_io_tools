2011fe05194da8f7907a07c4fe577ea2fbc7b65c
Rares Vernica <rvernica@gmail.com>
2018-02-19 11:25:49 -0800

    Filter out "Failed query id:" from test output

--- tests/test_arrow.sh
+++ tests/test_arrow.sh
@@ -11,7 +11,8 @@ $IQ "aio_save(apply(build(<x:int64>[i=-9:9], i), j, iif(i%2=0, i, int64(null))),
 python -c "import pyarrow; print(pyarrow.open_stream('/tmp/1').read_all().to_pandas())" >> $TEST_OUT
 
 $IQ "aio_save(build(<x:uint64>[i=0:0], i), '/tmp/1', 'format=arrow')" 2>&1 \
-    |  sed --expression='s/ line: [0-9]\+//g' >> $TEST_OUT                 \
+    |  sed --expression='s/ line: [0-9]\+//g'                              \
+    |  grep --invert-match "Failed query id:" >> $TEST_OUT                 \
     || echo "expected exception"
 
 
