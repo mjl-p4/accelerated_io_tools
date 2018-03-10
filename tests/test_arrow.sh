9f34e916cc070f6dba3f6bdf9799a2a9cc328a59
Rares Vernica <rvernica@gmail.com>
2018-03-10 11:54:58 +0200

    Add empty chunks test

--- tests/test_arrow.sh
+++ tests/test_arrow.sh
@@ -14,23 +14,29 @@ function iq() {
 
 : > $TEST_OUT                   # Reset output file
 
+
+echo -e "\nI. Basic"
+echo "1. Single chunk"
 iq "aio_save(apply(build(<x:int64>[i=-10:10], i), y, iif(i%2=0, i, int64(null))), '$F', 'format=arrow')"
 python -c "import pyarrow; print(pyarrow.open_stream('$F').read_all().to_pandas())" \
     >> $TEST_OUT
 
+echo "2. Multiple chunks"
 iq "aio_save(apply(build(<x:int64>[i=1:100:0:20], i), y, iif(i%2=0, i, int64(null))), '$F', 'format=arrow')"
 python -c "import pyarrow; print(pyarrow.open_stream('$F').read_all().to_pandas().sort_values('x').to_string(index=False))" \
     >> $TEST_OUT
 
+echo "3. string"
 iq "aio_save(apply(build(<x:int64>[i=1:20:0:4], i), y, iif(i%2=0, string(i), string(null))), '$F', 'format=arrow')"
 python -c "import pyarrow; print(pyarrow.open_stream('$F').read_all().to_pandas().sort_values('x').to_string(index=False))" \
     >> $TEST_OUT
 
-$IQ "aio_save(build(<x:uint64>[i=0:0], i), '$F', 'format=arrow')" 2>&1 \
-    |  sed --expression='s/ line: [0-9]\+//g'                          \
-    |  grep --invert-match "Failed query id:" >> $TEST_OUT             \
-    || echo "expected exception"
+echo "4. Empty chunk"
+iq "aio_save(filter(build(<x:int64>[i=1:20:0:4], i), x < 6 or x > 14), '$F', 'format=arrow')"
+python -c "import pyarrow; print(pyarrow.open_stream('$F').read_all().to_pandas().sort_values('x').to_string(index=False))" \
+    >> $TEST_OUT
 
+echo "5. Different output sinks"
 $IQ "aio_save(build(<x:int64>[i=0:0], i), 'console', 'format=arrow')" \
     >> $TEST_OUT
 $IQ "aio_save(build(<x:int64>[i=0:0], i), 'stdout', 'format=arrow')" \
@@ -39,7 +45,14 @@ $IQ "aio_save(build(<x:int64>[i=0:0], i), 'stderr', 'format=arrow')" \
     >> $TEST_OUT
 
 
-echo "I. int64, int64(null)"
+echo -e "\nII. Exceptions"
+$IQ "aio_save(build(<x:uint64>[i=0:0], i), '$F', 'format=arrow')" 2>&1 \
+    |  sed --expression='s/ line: [0-9]\+//g'                          \
+    |  grep --invert-match "Failed query id:" >> $TEST_OUT             \
+    || echo "expected exception"
+
+
+echo -e "\nIII. int64, int64(null)"
 echo "1. store"
 time $IQ "set no fetch; store(apply(build(<x:int64 not null>[i=1:3100000], i), y, iif(i%2=0, i * i, int64(null))), foo)" \
      >> $TEST_OUT
@@ -81,7 +94,7 @@ time python -c "import scidbpy; print(len(scidbpy.connect().arrays.foo.fetch(att
 iq "remove(foo)"
 
 
-echo "II. int64, string(null)"
+echo -e "\nIV. int64, string(null)"
 echo "1. store"
 time $IQ "set no fetch; store(apply(build(<x:int64 not null>[i=1:2700000], i), y, iif(i%2=0, string(i * i), string(null))), foo)" \
      >> $TEST_OUT
