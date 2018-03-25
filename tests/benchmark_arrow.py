90ded70c8b369c9983f53e28e9b51324df6f627e
Rares Vernica <rvernica@gmail.com>
2018-03-25 13:14:49 -0700

    Add chunk size and buffer size to benchmark

--- tests/benchmark_arrow.py
+++ tests/benchmark_arrow.py
@@ -6,6 +6,8 @@ import timeit
 
 ar_names = ('bm_fix', 'bm_var')
 ar_schemas = [None, None]
+chunk_size = 100000
+buffer_size = 8 * 1024 * 1024
 
 
 def setup(mb):
@@ -14,7 +16,7 @@ def setup(mb):
     db = scidbpy.connect()
 
     ar_name = ar_names[0]
-    db.build('<z:int64 not null>[i=1:{}]'.format(cnt),
+    db.build('<z:int64 not null>[i=1:{}:0:{}]'.format(cnt, chunk_size),
              'random()'
              ).apply('y', 'int64(random())',
                      'x', 'int64(random())',
@@ -22,14 +24,18 @@ def setup(mb):
                      ).store(ar_name)
     ar_schemas[0] = db.arrays[ar_name].schema()
     scidb_bytes_fix = db.summarize(ar_name).project('bytes')['bytes'][0]
-    mem_bytes_fix = db.scan(ar_name).fetch(atts_only=True,
-                                           as_dataframe=False).nbytes
+    chunks = db.summarize(
+        ar_name, 'by_attribute:true').project(
+            'chunks').fetch(atts_only=True, as_dataframe=False)[0][0][1]
+    mem_bytes_fix = 0
+    # mem_bytes_fix = db.scan(ar_name).fetch(atts_only=True,
+    #                                        as_dataframe=False).nbytes
     db.aio_save(
         ar_name, "'/dev/shm/{}'".format(ar_name), "'format=arrow'").fetch()
     file_bytes_fix = os.path.getsize('/dev/shm/' + ar_name)
 
     ar_name = ar_names[1]
-    db.build('<z:int64 not null>[i=1:{}]'.format(cnt),
+    db.build('<z:int64 not null>[i=1:{}:0:{}]'.format(cnt, chunk_size),
              'random()'
              ).apply('y', 'int64(random())',
                      'x', 'int64(random())',
@@ -38,8 +44,9 @@ def setup(mb):
                      ).store(ar_name)
     ar_schemas[1] = db.arrays[ar_name].schema()
     scidb_bytes_var = db.summarize(ar_name).project('bytes')['bytes'][0]
-    mem_bytes_var = db.scan(ar_name).fetch(atts_only=True,
-                                           as_dataframe=False).nbytes
+    mem_bytes_var = 0
+    # mem_bytes_var = db.scan(ar_name).fetch(atts_only=True,
+    #                                        as_dataframe=False).nbytes
     db.aio_save(
         ar_name, "'/dev/shm/{}'".format(ar_name), "'format=arrow'").fetch()
     file_bytes_var = os.path.getsize('/dev/shm/' + ar_name)
@@ -47,22 +54,30 @@ def setup(mb):
     print("""
 Setup
 ===
-Target size:    {:6.2f} MB
-Number of runs: {:3d}
+Number of runs:    {:7d}
+Target size:       {:10.2f} MB
+Buffer size:       {:10.2f} MB
+Chunk size:        {:7d}
+Number of records: {:7d}
+Number of chunks:  {:7d}
 
 Fix Size Schema (int64 only)
 ---
-SciDB size:     {:6.2f} MB
-In-memory size: {:6.2f} MB
-File size:      {:6.2f} MB
+SciDB size:        {:7.2f} MB
+In-memory size:    {:7.2f} MB
+File size:         {:7.2f} MB
 
 Variable Size Schema (int64 and string)
 ---
-SciDB size:     {:6.2f} MB
-In-memory size: {:6.2f} MB
-File size:      {:6.2f} MB""".format(
-    mb,
+SciDB size:        {:7.2f} MB
+In-memory size:    {:7.2f} MB
+File size:         {:7.2f} MB""".format(
     runs,
+    mb,
+    buffer_size / 1024. / 1024,
+    chunk_size,
+    cnt,
+    chunks,
     scidb_bytes_fix / 1024. / 1024,
     mem_bytes_fix / 1024. / 1024,
     file_bytes_fix / 1024. / 1024,
@@ -87,10 +102,11 @@ Fix Size Schema (int64 only)
 ---""")
     fmt = '(int64,int64,int64,int64)'
     stmt = """
-db.iquery("aio_save({ar_name}, '/dev/shm/{ar_name}', 'format={fmt}')",
+db.iquery("aio_save({ar_name}, '/dev/shm/{ar_name}', 'format={fmt}', 'buffer_size={buffer_size}')",
           fetch = True)""".format(
               ar_name=ar_name,
-              fmt=fmt)
+              fmt=fmt,
+              buffer_size=buffer_size)
     rt = timeit.Timer(stmt=stmt, setup=setup).timeit(number=runs) / runs
     print("""\
 Binary: {:6.2f} seconds {:6.2f} MB/second""".format(
@@ -98,10 +114,11 @@ Binary: {:6.2f} seconds {:6.2f} MB/second""".format(
 
     fmt = 'arrow'
     stmt = """
-db.iquery("aio_save({ar_name}, '/dev/shm/{ar_name}', 'format={fmt}')",
+db.iquery("aio_save({ar_name}, '/dev/shm/{ar_name}', 'format={fmt}', 'buffer_size={buffer_size}')",
           fetch = True)""".format(
               ar_name=ar_name,
-              fmt=fmt)
+              fmt=fmt,
+              buffer_size=buffer_size)
     rt = timeit.Timer(stmt=stmt, setup=setup).timeit(number=runs) / runs
     print("""\
 Arrow:  {:6.2f} seconds {:6.2f} MB/second""".format(
@@ -113,10 +130,11 @@ Variable Size Schema (int64 and string)
 ---""")
     fmt = '(int64,int64,int64,int64,string)'
     stmt = """
-db.iquery("aio_save({ar_name}, '/dev/shm/{ar_name}', 'format={fmt}')",
+db.iquery("aio_save({ar_name}, '/dev/shm/{ar_name}', 'format={fmt}', 'buffer_size={buffer_size}')",
           fetch = True)""".format(
               ar_name=ar_name,
-              fmt=fmt)
+              fmt=fmt,
+              buffer_size=buffer_size)
     rt = timeit.Timer(stmt=stmt, setup=setup).timeit(number=runs) / runs
     print("""\
 Binary: {:6.2f} seconds {:6.2f} MB/second""".format(
@@ -124,10 +142,11 @@ Binary: {:6.2f} seconds {:6.2f} MB/second""".format(
 
     fmt = 'arrow'
     stmt = """
-db.iquery("aio_save({ar_name}, '/dev/shm/{ar_name}', 'format={fmt}')",
+db.iquery("aio_save({ar_name}, '/dev/shm/{ar_name}', 'format={fmt}', 'buffer_size={buffer_size}')",
           fetch = True)""".format(
               ar_name=ar_name,
-              fmt=fmt)
+              fmt=fmt,
+              buffer_size=buffer_size)
     rt = timeit.Timer(stmt=stmt, setup=setup).timeit(number=runs) / runs
     print("""\
 Arrow:  {:6.2f} seconds {:6.2f} MB/second""".format(
@@ -213,6 +232,6 @@ if __name__ == "__main__":
     db = setup(mb)
 
     save(mb, runs)
-    download(mb, runs)
+    # download(mb, runs)
 
     cleanup(db)
