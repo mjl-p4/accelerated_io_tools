a6efd8de521f173080358539c129728cdeaeb3c4
Rares Vernica <rvernica@gmail.com>
2020-06-28 17:17:05 -0700

    Skip installing Arrow
    * Arrow is installed by extra-scidb-libs install.sh script

--- tests/docker-install.sh
+++ tests/docker-install.sh
@@ -13,60 +13,16 @@ then
     # yum install --assumeyes                                                    \
     #     https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
 
-    ## https://github.com/red-data-tools/packages.red-data-tools.org#centos
-    # yum install --assumeyes \
-    #     https://packages.red-data-tools.org/centos/red-data-tools-release-1.0.0-1.noarch.rpm
-
-    ## Use Bintray packages:
-    ## - Compiled without ORC support (avoid Protocol Buffers conflict)
-    # wget --output-document /etc/yum.repos.d/bintray-rvernica-rpm.repo \
-    #      https://bintray.com/rvernica/rpm/rpm
-
-    yum install --assumeyes https://apache.bintray.com/arrow/centos/$(
-        cut --delimiter : --fields 5 /etc/system-release-cpe
-        )/apache-arrow-release-latest.rpm
-
-    for pkg in centos-release-scl arrow-devel-$ARROW_VER libpqxx-devel python27
+    for pkg in centos-release-scl libpqxx-devel python27
     do
         yum install --assumeyes $pkg
     done
 
     source /opt/rh/python27/enable
 else
-    ## https://github.com/red-data-tools/packages.red-data-tools.org#ubuntu
-    ## No packages for Debian jessie, use Ubuntu trusty
-    # cat <<APT_LINE | tee /etc/apt/sources.list.d/red-data-tools.list
-    # deb https://packages.red-data-tools.org/ubuntu/ trusty universe
-    # APT_LINE
-    # apt-get update
-    # apt-get install                                                      \
-    #         --assume-yes --no-install-recommends --allow-unauthenticated \
-    #         red-data-tools-keyring
-
-    ## Use Bintray packages:
-    ## - Compiled with g++ 4.9
-    ## - Compiled without ORC support (avoid Protocol Buffers conflict)
-    # cat <<APT_LINE | tee /etc/apt/sources.list.d/bintray-rvernica.list
-    # deb https://dl.bintray.com/rvernica/deb trusty universe
-    # APT_LINE
-    # apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 46BD98A354BA5235
-
-    codename=`lsb_release --codename --short`
-    if [ "$codename" = "stretch" ]
-    then
-        cat > /etc/apt/sources.list.d/backports.list <<EOF
-deb http://deb.debian.org/debian $codename-backports main
-EOF
-    fi
-    wget https://apache.bintray.com/arrow/$(
-        echo $id | tr 'A-Z' 'a-z'
-        )/apache-arrow-archive-keyring-latest-$codename.deb
-    apt install --assume-yes ./apache-arrow-archive-keyring-latest-$codename.deb
-
     apt-get update
     apt-get install                              \
             --assume-yes --no-install-recommends \
-            libarrow-dev=$ARROW_VER-1            \
             libpqxx-dev
 fi
 
@@ -99,6 +55,7 @@ fi
 # Compile and install plugin
 scidbctl.py stop $SCIDB_NAME
 make --directory /accelerated_io_tools
-cp /accelerated_io_tools/libaccelerated_io_tools.so $SCIDB_INSTALL_PATH/lib/scidb/plugins/
+cp /accelerated_io_tools/libaccelerated_io_tools.so     \
+   $SCIDB_INSTALL_PATH/lib/scidb/plugins/
 scidbctl.py start $SCIDB_NAME
 iquery --afl --query "load_library('accelerated_io_tools')"
